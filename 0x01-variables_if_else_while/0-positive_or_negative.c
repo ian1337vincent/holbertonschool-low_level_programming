125927745aa7717f1fbf5ae9dad108ae150e293b
ian1337vincent <vincentian173@gmail.com>
2020-06-15 21:51:23 +0000

    added return value to main function

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-
+/**
+* main - prints number
+*
+* Return: 0
+*/
 /* betty style doc for function main goes there */
 int main(void)
 {
@@ -10,12 +14,14 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n >= 0)
+	{
+	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n == 0)
 		printf("%d is zero\n", n);
-	else if (n <= 0)
+	else if (n < 0)
 		printf("%d is negative\n", n);
+	}
 	/* your code goes there */
 	return (0);
 }
