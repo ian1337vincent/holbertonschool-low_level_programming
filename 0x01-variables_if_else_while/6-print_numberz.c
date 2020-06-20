fe9e8921119a705c769715eb447b9bf129891011
ian1337vincent <vincentian173@gmail.com>
2020-06-20 23:22:39 +0000

    program that prints 0-9 with char varible

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - prints 0-9
+*
+* Return: 0
+*/
+int main(void)
+{
+	char d = '0';
+
+	while (d <= '9')
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
