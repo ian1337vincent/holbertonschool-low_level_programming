c876e0f3faf9c383b626cda72cf5573847fb2a6a
ian1337vincent <vincentian173@gmail.com>
2020-06-20 23:05:44 +0000

    program that prints 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - prints single digit #'s of base 10,
+* starting from 0 followed by a new line
+*
+* Return: 0
+*/
+int main(void)
+{
+	printf("0123456789\n");
+
+	return (0);
+}
