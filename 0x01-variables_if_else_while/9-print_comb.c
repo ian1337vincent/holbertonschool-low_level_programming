8b68c293fa7f9b435b183f6a6ed714e2ab026085
ian1337vincent <vincentian173@gmail.com>
2020-06-21 03:41:13 +0000

    program that prints single digit numbers followed by comma and space in ascending order

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - prints all possible combos of 0-9
+*
+* Return: 0
+*/
+int main(void)
+{
+	int digit = '0';
+	int comma = ',';
+	int space = ' ';
+
+	while (digit <= '8')
+	{
+		putchar(digit);
+		putchar(comma);
+		putchar(space);
+		++digit;
+	}
+	putchar('9');
+	return (0);
+}
