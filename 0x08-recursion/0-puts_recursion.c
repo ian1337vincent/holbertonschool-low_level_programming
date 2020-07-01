6e331fe1e283a6ad443993052a92bd0a3c7cd72b
ian1337vincent <vincentian173@gmail.com>
2020-07-01 19:37:07 +0000

    function that prints a string recursively

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* _puts_recursion - prints a string
+* @s: string
+*/
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	s++;
+	if (*s > 0)
+	{
+		_puts_recursion(s);
+	}
+}
