0d134094907b7e0b27fab8967da156d587689305
ian1337vincent <vincentian173@gmail.com>
2020-07-05 21:01:38 +0000

    function that converts a string to an integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+* _atoi - convert a string to an integer
+* @s: pointer
+* Return: s, 0 if no numbers in string
+*/
+int _atoi(char *s)
+{
+	return (s);
+	return (0);
+}
