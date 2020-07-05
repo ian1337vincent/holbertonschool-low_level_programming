a63c9a6b55dbfed84a4ba2a38a98dceca3d27869
ian1337vincent <vincentian173@gmail.com>
2020-07-05 20:42:48 +0000

    added include guards

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,3 +1,6 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
 int _putchar(char c);
 
 void reset_to_98(int *n);
@@ -5,3 +8,10 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 
 int _strlen(char *s);
+
+void _puts(char *str);
+
+void print_rev(char *s);
+
+#endif /* HOLBERTON_H */
+
